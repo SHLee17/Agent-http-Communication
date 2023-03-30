741da2f290c268650762a98246c5b217a4b84836
SHLee17 <azurite17@gmail.com>
2023-03-31 05:34:34 +0900

    First

---
+++ Server/Server/Program.cs
@@ -0,0 +1,125 @@
+﻿using System.Text;
+using System.Timers;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static object previousValue = null;
+        static async Task Main(string[] args)
+        {
+
+            string folderPath = Path.Combine(Directory.GetCurrentDirectory(), "Upload");
+            string downloadFolderPath = Path.Combine(Directory.GetCurrentDirectory(), "Download");
+            NewFolder(folderPath, "Upload");
+            NewFolder(downloadFolderPath, "Download");
+
+            string[] filePaths = Directory.GetFiles(downloadFolderPath, "*.txt");
+            DateTime earliestModifiedDate = DateTime.MaxValue;
+            string earliestFilePath = "";
+
+            foreach (string filePath in filePaths)
+            {
+                FileInfo fileInfo = new FileInfo(filePath);
+                if (fileInfo.LastWriteTime < earliestModifiedDate)
+                {
+                    earliestFilePath = filePath;
+                    earliestModifiedDate = fileInfo.LastWriteTime;
+                }
+            }
+
+            if (earliestModifiedDate < DateTime.MaxValue)
+                previousValue = await File.ReadAllTextAsync(earliestFilePath);
+
+            // 폴더의 파일 변경을 모니터링하기 위한 FileSystemWatcher 개체 생성
+            FileSystemWatcher watcher = new FileSystemWatcher();
+            watcher.Path = folderPath;
+            watcher.Filter = "*.txt"; // 확장자 필터
+            watcher.NotifyFilter = NotifyFilters.LastWrite;
+            watcher.Changed += OnChanged;
+            watcher.EnableRaisingEvents = true;
+
+            using var httpClient = new HttpClient();
+            string previousFilePath = earliestFilePath;
+
+            while (true)
+            {
+                var response = await httpClient.GetAsync("http://r741.realserver2.com/api/post.php");
+                response.EnsureSuccessStatusCode();
+                string currentValue = await response.Content.ReadAsStringAsync();
+                if (previousFilePath == null || !File.Exists(previousFilePath) || currentValue != File.ReadAllText(previousFilePath))
+                {
+                    string fileName = DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+                    string filePath = Path.Combine(downloadFolderPath, fileName);
+                    File.WriteAllText(filePath, currentValue);
+                    previousFilePath = filePath;
+                }
+                await Task.Delay(TimeSpan.FromSeconds(1));
+            }
+
+            //System.Timers.Timer timer = new System.Timers.Timer(10000);
+            //timer.Elapsed += OnTimerElapsed;
+            //timer.Start();
+
+            //Console.WriteLine("Press enter to exit.");
+            //Console.ReadLine();
+        }
+        static void NewFolder(string path, string name)
+        {
+            if (!Directory.Exists(path))
+            {
+                // 폴더가 없는 경우 폴더 만들기
+                Directory.CreateDirectory(path);
+                Console.WriteLine($"New {name} folder created.");
+            }
+            else
+            {
+                Console.WriteLine($"{name} Folder already exists.");
+            }
+            Console.WriteLine($"{name} Folder path: " + path);
+        }
+        static async void OnChanged(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine($"{e.FullPath} 생성되었습니다.");
+
+            // 파일 내용을 읽고 문자열로 변환
+            string fileContents = File.ReadAllText(e.FullPath);
+
+            using var httpClient = new HttpClient();
+
+            // HTTP 요청을 사용하여 홈 페이지로 문자열 전송
+                var content = new StringContent(fileContents, Encoding.UTF8, "text/plain");
+                var response = await httpClient.PostAsync("http://r741.realserver2.com/api/testJSON.php", content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseBody = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(responseBody);
+                }
+                else
+                    Console.WriteLine($"Error: {response.StatusCode}");
+        }
+
+        static async void OnTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            // HTTP 요청을 사용하여 홈페이지에서 보낸 문자열 값 확인
+            //using (HttpClient client = new HttpClient())
+            //{
+            //    var response = await client.GetAsync("http://r741.realserver2.com/api/post.php");
+            //    string currentValue = await response.Content.ReadAsStringAsync();
+            //    if (previousValue != currentValue)
+            //        previousValue = currentValue;
+            //    else return;
+            //    // 현재 날짜 및 시간을 기준으로 고유한 파일 이름 생성
+            //    string fileName = "Download_" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".txt";
+            //    string filePath = Path.Combine(downloadFolderPath, fileName);
+
+            //    File.WriteAllText(filePath, currentValue);
+            //}
+        }
+
+    }
+
+}
+
+
+
